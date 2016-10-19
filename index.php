0abaffd27a0339190cc1c2d096d7a6efa5e4a343
Flo LeBlanc <Flo LeBlanc>
2016-10-19 15:53:53 +0200

    introduce square and output all triangles and squares via foreach

--- index.php
+++ index.php
@@ -15,8 +15,19 @@ namespace PHPGrundlagen\GeometricForms;
  */
 class Triangle
 {
-    public $side_length;
-    public $color;
+    /* @var int $side_lenght */
+    private $side_length;
+
+    /* @var string $color */
+    private $color;
+
+    /**
+     * @return string color as hexcode
+     */
+    public function getColor()
+    {
+        return $this->color;
+    }
 
     /**
      * Triangle constructor.
@@ -38,20 +49,77 @@ class Triangle
     }
 }
 
+/**
+ * Class Square
+ *
+ * represents a square
+ */
+class Square
+{
+    /* @var int $side_lenght */
+    private $side_length;
+
+    /* @var string $color */
+    private $color;
+
+    /**
+     * @return string color as hexcode
+     */
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    /**
+     * Triangle constructor.
+     * @param int $side_length
+     * @param string $color
+     */
+    public function __construct(int $side_length, string $color)
+    {
+        $this->side_length = $side_length;
+        $this->color = $color;
+    }
+
+    /**
+     * @return float perimeter
+     */
+    public function calculatePerimeter() : float
+    {
+        return 4*$this->side_length;
+    }
+}
+
 $green = "#00FF00";
 $red = "#FF0000";
 $blue = "#0000FF";
 
-$triangle1 = new Triangle(4, $green);
-$triangle2 = new Triangle(2, $blue);
-
-echo sprintf("Es gibt ein Dreieck mit Umfang %d cm und der Farbe %s\n",
-    $triangle1->calculatePerimeter(),
-    $triangle1->color
+$triangles = array(
+    new Triangle(4, $green),
+    new Triangle(2, $blue),
+    new Triangle(3, $red)
 );
-echo "Es gibt ein Quadrat mit Umfang ".(4*3)." cm und der Farbe grün.\n ";
-echo "Es gibt ein Dreieck mit Umfang ".(3*2)." cm und der Farbe blau.\n ";
-echo "Es gibt ein Dreieck mit Umfang ".(3*3)." cm und der Farbe rot.\n ";
-echo "Es gibt ein Quadrat mit Umfang ".(4*5)." cm und der Farbe rot.\n ";
 
+foreach($triangles as $triangle)
+{
+    /* @var Triangle $triangle */
+    echo sprintf(
+        "Es gibt ein Dreieck mit Umfang %d cm und der Farbe %s\n",
+        $triangle->calculatePerimeter(),
+        $triangle->getColor()
+    );
+}
 
+$squares = array(
+    new Square(3, $green),
+    new Square(5, $red)
+);
+
+foreach($squares as $square){
+    /* @var Square $square */
+    echo sprintf(
+        "Es gibt ein Quadrat mit Umfang %d cm und der Farbe %s\n",
+        $square->calculatePerimeter(),
+        $square->getColor()
+    );
+}
