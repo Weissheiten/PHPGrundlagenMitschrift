6c397f9699863d8d9e1ce3022ac9b84de39b65b8
Flo LeBlanc <Flo LeBlanc>
2016-10-19 14:39:25 +0200

    first prodecural draft for the geometric forms program

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: FloLeBlanc
+ * Date: 19.10.2016
+ * Time: 13:59
+ */
+
+echo "Es gibt ein Dreieck mit Umfang ".(3*4)." cm und der Farbe grün.\n";
+echo "Es gibt ein Quadrat mit Umfang ".(4*3)." cm und der Farbe grün.\n ";
+echo "Es gibt ein Dreieck mit Umfang ".(3*2)." cm und der Farbe blau.\n ";
+echo "Es gibt ein Dreieck mit Umfang ".(3*3)." cm und der Farbe rot.\n ";
+echo "Es gibt ein Quadrat mit Umfang ".(4*5)." cm und der Farbe rot.\n ";
+
+
